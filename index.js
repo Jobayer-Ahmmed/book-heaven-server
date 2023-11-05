20f6e2acbb85967d5ca4bfc2adf6daf97c06ece2
Jobayer-Ahmmed <jobayer.ahmmed.jas@gmail.com>
2023-11-05 19:28:06 +0600

    creating categoryCollection & get api of category

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ import "dotenv/config"
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({
+  origin:["http://localhost:5173"],
+  credentials: true
+}))
 app.use(express.json())
 app.use(cookieParser())
 
@@ -24,6 +27,7 @@ async function run() {
   try {
     const libraryDB = client.db("libraryDB")
     const booksCollection = libraryDB.collection("booksCollection")
+    const categoryCollection = libraryDB.collection("categoryCollection")
 
 
     app.get("/", (req, res)=>{
@@ -32,6 +36,12 @@ async function run() {
     app.get("/books", async(req, res)=>{
       console.log("i am books")
       const cursor = booksCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+    app.get("/category", async(req, res)=>{
+      console.log("i am from  category")
+      const cursor = categoryCollection.find()
       const result = await cursor.toArray()
       res.send(result)
     })
